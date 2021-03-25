2ab9bd13acfb49306a3669aa74b4ba96f6883926
Prabin Singh <pravin544@gmail.com>
2021-03-25 00:22:00 -0500

    removed unused folder

---
+++ src/components/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import HornedBeasts from './HornedBeasts'
+import { CardDeck } from 'react-bootstrap';
+import animalArray from '../data.json'
+
+
+class Main extends React.Component{
+    render () {
+        return(
+            <CardDeck>
+                {animalArray.map(animal=>(
+                    <HornedBeasts  
+                        title = {animal.title} 
+                        description = {animal.description}
+                        imgUrl = {animal.image_url}
+                        keyword={animal.keyword}
+                        horns={animal.horns}
+                    />
+                ))}
+            </CardDeck>
+        )
+    }
+}
+
+export default Main;
