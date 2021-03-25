c2d384ac7c6e3589c9098f5e62943fbcc7004fed
Prabin Singh <pravin544@gmail.com>
2021-03-24 23:35:10 -0500

    added bootstrap

---
+++ hornedbeastsreact/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer'
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+
+}
+
+export default App;
